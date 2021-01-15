1e3e2369f93984f1bf4b1be5d1fe3ce16d657982
HywenSettler <samratkadian99@gmail.com>
2021-01-15 20:46:40 +0530

    fix ngo side

--- src/components/FoodSearch.js
+++ src/components/FoodSearch.js
@@ -4,7 +4,7 @@ import './Dashboard/Dashboard.css';
 
 const FoodSearch = () => {
   return (
-    <div className="row">
+    <div className="row scroll-div">
       <div className="card card-div">
         <img
           src="https://res.cloudinary.com/dc2o7coc1/image/upload/v1608285294/food-waste-mgmt/food-items/breakfast/milk.jpg"
@@ -15,6 +15,39 @@ const FoodSearch = () => {
           alt="..."
         />
         <div className="card-body d-flex justify-content-center">Milk</div>
+      </div>
+      <div className="card card-div">
+        <img
+          src="https://res.cloudinary.com/dc2o7coc1/image/upload/v1608285289/food-waste-mgmt/food-items/breakfast/pav-bhaji.jpg"
+          className="card-img-top img-size"
+          style={{
+            borderRadius: '20px 20px 0px 0px'
+          }}
+          alt="..."
+        />
+        <div className="card-body d-flex justify-content-center">Pav Bhaji</div>
+      </div>
+      <div className="card card-div">
+        <img
+          src="https://res.cloudinary.com/dc2o7coc1/image/upload/v1608285289/food-waste-mgmt/food-items/breakfast/pav-bhaji.jpg"
+          className="card-img-top img-size"
+          style={{
+            borderRadius: '20px 20px 0px 0px'
+          }}
+          alt="..."
+        />
+        <div className="card-body d-flex justify-content-center">Pav Bhaji</div>
+      </div>
+      <div className="card card-div">
+        <img
+          src="https://res.cloudinary.com/dc2o7coc1/image/upload/v1608285289/food-waste-mgmt/food-items/breakfast/pav-bhaji.jpg"
+          className="card-img-top img-size"
+          style={{
+            borderRadius: '20px 20px 0px 0px'
+          }}
+          alt="..."
+        />
+        <div className="card-body d-flex justify-content-center">Pav Bhaji</div>
       </div>
       <div className="card card-div">
         <img
